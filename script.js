0fdc8a8956ea1dc9fce2c14d26d877ef27340472
desai2424 <154447814+desai2424@users.noreply.github.com>
2023-12-20 15:21:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ document.getElementById("myForm").addEventListener("submit", function (e) {
         data[key] = value;
     });
 
-    fetch('https://https://lpsxfflmyvgagrhzdjgs.supabase.co.supabase.co/rest/v1/UserResponses', {
+    fetch('https://YOUR_PROJECT_URL.supabase.co/rest/v1/UserResponses', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxwc3hmZmxteXZnYWdyaHpkamdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwOTYzNzgsImV4cCI6MjAxODY3MjM3OH0.azuRgd-k94VxWTwxWU8LzlLa5ID4FHVdPg18uIB1kPE'
+            'apikey': 'YOUR_API_KEY'
         },
         body: JSON.stringify(data)
     })
     .then(response => response.json())
     .then(data => {
         console.log('Data sent successfully:', data);
+        // Display success message
+        document.getElementById("successMessage").style.display = "block";
         // You can add further handling or redirection here if needed.
     })
     .catch(error => {
